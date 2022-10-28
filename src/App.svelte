1a557040394b38f40743feae21879e27bd0c5eb9
Pawan <pawandeepk@my.com>
2022-10-28 11:24:05 +0100

    added animation package

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,35 @@
 <script>
   import Card from "./components/generic/Card.svelte";
   import Header from "./components/layout/Header.svelte";
+  import Saos from "saos";
 </script>
 
 <!-- HTML -->
 <Header />
 
 <Card>
-  <div class="text1">
-    <p>
-      Almost every business in the country is a small business.
-      <b>Who knew?</b>
-    </p>
-  </div>
+  <Saos
+    animation={"fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both"}
+  >
+    <div class="text1">
+      <p>Almost every business in the country is a small business.</p>
+    </div>
+  </Saos>
 
-  <div class="text2">
+  <Saos
+    animation={"puff-in-center 1s cubic-bezier(0.470, 0.000, 0.745, 0.715) both"}
+    animation_out={"slide-out-elliptic-top-bck 0.7s ease-in both"}
+    top={250}
+    bottom={250}
+  >
+    <div class="text2">
+      <p>
+        <b>Who knew?</b>
+      </p>
+    </div>
+  </Saos>
+
+  <div class="text3">
     <p>
       They provide services that make our day to day lives work & are the
       lifeblood of our economy.
@@ -30,14 +45,242 @@
   .text1 {
     text-align: left;
     font-family: serif;
-    max-width: 200px;
+    max-width: 500px;
     padding-left: 100px;
-    font-size: 25px;
+    font-size: 50px;
   }
 
   .text2 {
-    max-width: 200px;
-    padding-right: 100px;
-    font-size: 25px;
+    text-align: left;
+    font-family: serif;
+    max-width: 500px;
+    padding-left: 100px;
+    font-size: 50px;
+  }
+
+  /* ----------------------------------------------
+ * Animations below by Animista on 2020-7-11
+ * Licensed under FreeBSD License.
+ * See http://animista.net/license for more info. 
+ * w: http://animista.net, t: @cssanimista
+ * ---------------------------------------------- */
+
+  @keyframes -global-scale-in-center {
+    0% {
+      transform: scale(0);
+      opacity: 1;
+    }
+    100% {
+      transform: scale(1);
+      opacity: 1;
+    }
+  }
+
+  @keyframes -global-rotate-in-center {
+    0% {
+      transform: rotate(-360deg);
+      opacity: 0;
+    }
+    100% {
+      transform: rotate(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes -global-slide-in-top {
+    0% {
+      transform: translateY(-1000px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes -global-slide-in-fwd-tr {
+    0% {
+      transform: translateZ(-1200px) translateY(-700px) translateX(500px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateZ(0) translateY(0) translateX(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes -global-slide-in-elliptic-top-fwd {
+    0% {
+      transform: translateY(-600px) rotateX(-30deg) scale(0);
+      transform-origin: 50% 100%;
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0) rotateX(0) scale(1);
+      transform-origin: 50% 1400px;
+      opacity: 1;
+    }
+  }
+
+  @keyframes -global-roll-in-left {
+    0% {
+      transform: translateX(-800px) rotate(-540deg);
+      opacity: 0;
+    }
+    100% {
+      transform: translateX(0) rotate(0deg);
+      opacity: 1;
+    }
+  }
+
+  @keyframes -global-roll-in-blurred-left {
+    0% {
+      transform: translateX(-1000px) rotate(-720deg);
+      filter: blur(50px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateX(0) rotate(0deg);
+      filter: blur(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes -global-tilt-in-fwd-tr {
+    0% {
+      transform: rotateY(20deg) rotateX(35deg) translate(300px, -300px)
+        skew(-35deg, 10deg);
+      opacity: 0;
+    }
+    100% {
+      transform: rotateY(0) rotateX(0deg) translate(0, 0) skew(0deg, 0deg);
+      opacity: 1;
+    }
+  }
+
+  @keyframes -global-swing-in-top-fwd {
+    0% {
+      transform: rotateX(-100deg);
+      transform-origin: top;
+      opacity: 0;
+    }
+    100% {
+      transform: rotateX(0deg);
+      transform-origin: top;
+      opacity: 1;
+    }
+  }
+
+  @keyframes -global-puff-in-center {
+    0% {
+      transform: scale(2);
+      filter: blur(4px);
+      opacity: 0;
+    }
+    100% {
+      transform: scale(1);
+      filter: blur(0px);
+      opacity: 1;
+    }
+  }
+
+  @keyframes -global-fade-in {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  /* EXITS */
+  @keyframes -global-scale-out-center {
+    0% {
+      transform: scale(1);
+      opacity: 1;
+    }
+    100% {
+      transform: scale(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes -global-rotate-out-center {
+    0% {
+      transform: rotate(0);
+      opacity: 1;
+    }
+    100% {
+      transform: rotate(-360deg);
+      opacity: 0;
+    }
+  }
+
+  @keyframes -global-rotate-out-2-cw {
+    0% {
+      transform: rotate(0);
+      opacity: 1;
+    }
+    100% {
+      transform: rotate(45deg);
+      opacity: 0;
+    }
+  }
+
+  @keyframes -global-swirl-out-bck {
+    0% {
+      transform: rotate(0) scale(1);
+      opacity: 1;
+    }
+    100% {
+      transform: rotate(-540deg) scale(0);
+      opacity: 0;
+    }
+  }
+
+  @keyframes -global-flip-out-hor-top {
+    0% {
+      transform: rotateX(0);
+      opacity: 1;
+    }
+    100% {
+      transform: rotateX(70deg);
+      opacity: 0;
+    }
+  }
+
+  @keyframes -global-slide-out-top {
+    0% {
+      transform: translateY(0);
+      opacity: 1;
+    }
+    100% {
+      transform: translateY(-1000px);
+      opacity: 0;
+    }
+  }
+
+  @keyframes -global-slide-out-fwd-center {
+    0% {
+      transform: translateZ(1);
+      opacity: 1;
+    }
+    100% {
+      transform: translateZ(600px);
+      opacity: 0;
+    }
+  }
+
+  @keyframes -global-slide-out-elliptic-top-bck {
+    0% {
+      transform: translateY(0) rotateX(0) scale(1);
+      transform-origin: 50% 1400px;
+      opacity: 1;
+    }
+    100% {
+      transform: translateY(-600px) rotateX(-30deg) scale(0);
+      transform-origin: 50% 100%;
+      opacity: 1;
+    }
   }
 </style>
